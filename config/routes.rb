067326d4706e2c9685861fc8855c10ae39684e72
Angela Coates <angela@angelamarii.com>
2017-05-04 15:03:55 -0400

    installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/landing'
+
   resources :clothings
+
+  root 'pages#landing'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
